3bfb9d3a1bf65506f952c013bc2a69fcf496f386
amjadPasha1227 <amjadpasha1227@gmail.com>
2022-06-01 17:12:05 +0530

    Practice file upload

---
+++ 00.Practice.js
@@ -0,0 +1,32 @@
+let SQRT_VALUE = document.getElementById("sqrt_value"),
+CBRT_VALUE = document.getElementById("cbrt_value"),
+SQRT_RESULT = document.getElementById("sqrt_result"),
+CBRT_RESULT = document.getElementById("cbrt_result"),
+SQR_VALUE = document.getElementById("sqr_value"),
+CUB_VALUE = document.getElementById("cub_value"),
+SQR_RESULT = document.getElementById("sqr_result"),
+CUB_RESULT = document.getElementById("cub_result"),
+ERR_MSG = document.getElementById("error_msg");
+
+ERR_MSG.style.display = 'none';
+
+SQR_VALUE.addEventListener("input", function(){
+    if(!isNaN(SQR_VALUE.value)){
+        ERR_MSG.style.display = 'none';
+        return SQR_RESULT.innerHTML = SQR_VALUE.value * SQR_VALUE.value;
+    }else{
+        ERR_MSG.style.display = 'inline-block';
+    }
+});
+
+CUB_VALUE.addEventListener("input", function(){
+    CUB_RESULT.innerHTML = CUB_VALUE.value * CUB_VALUE.value * CUB_VALUE.value;
+});
+
+SQRT_VALUE.addEventListener("input", function(){
+    SQRT_RESULT.innerHTML = Math.sqrt(SQRT_VALUE.value);
+});
+
+CBRT_VALUE.addEventListener("input", function(){
+    CBRT_RESULT.innerHTML = Math.cbrt(CBRT_VALUE.value);
+});

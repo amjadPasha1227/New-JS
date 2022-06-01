c6c77a02760cbf424f691e1d68bb778c0b3ee67e
amjadPasha <amjadpasha1227@gmail.com>
2022-06-01 23:50:28 +0530

    New Pages

--- 10.Conditional_Statement.js
+++ 10.Conditional_Statement.js
@@ -3,4 +3,39 @@
 // else
 // else if 
 // switch
+let number = 3;
+if(number > 0){
+    console.log("Positive Number");
+}else if(number < 0){
+    console.log("Negative Number");
+}else{
+    console.log("Zero");
+}
 
+const day = new Date().getDay();
+
+switch (day) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;      
+    default:
+        break;
+}

5b982c2646bea89de5b852ac81d53e06a8b87204
amjadPasha <amjadpasha1227@gmail.com>
2022-06-04 00:33:39 +0530

    New File

--- 11.loop.js
+++ 11.loop.js
@@ -2,9 +2,59 @@ let x = 3;
 x++;
 console.log(x);
 
-// for loop
+/*1. or loop
+ syntax:
+ for(initializer; condition; increment/decrement){
+    code to be executed
+}
+*/
 for(let i=1; i<=3; i++){
     console.log("Increment Value " + i);
 }
 
-// while loop
+/*2. whie loop
+ syntax:
+ initializer
+ while(condition){
+    code to be executed
+
+    increment/decrement
+ }
+*/
+let i=1;
+while(i<=3){
+    console.log("Increment Value " + i);
+
+    i++;
+}
+/*3. do whie loop
+ syntax:
+ initializer
+ do{
+    code to be executed
+
+    increment/decrement
+ }while(condition)
+*/
+let j=1;
+do{
+    console.log("Increment Value " + j);
+    j++;
+}
+while (j<=3);
+
+/*4. for of
+ syntax:
+ for (variable of iterable) {
+  statement
+}
+*/
+const iterable = [10, 20, 30];
+
+for (const value of iterable) {
+  console.log(value);
+}
+
+
+
+

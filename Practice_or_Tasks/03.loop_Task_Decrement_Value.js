6d4b7fdfbe3b6d109f19483f1e4f538119d1a372
amjadPasha <amjadpasha1227@gmail.com>
2022-06-05 02:20:29 +0530

    New folder system

---
+++ Practice_or_Tasks/03.loop_Task_Decrement_Value.js
@@ -0,0 +1,26 @@
+// for loop
+for (let i = 3; i >= 1; i--) {
+    console.log("Decrement Value " + i);
+}
+
+
+// while loop
+let i=3;
+while(i >= 1){
+    console.log("Decrement Value " + i);
+    i--;
+}
+
+// do while loop
+let j=3;
+do{
+    console.log("Decrement Value " + j);
+    j--;
+}
+while(j >= 1)
+
+// for of
+const arr = [1, 2, 3];
+for(var list of arr.reverse()){
+    console.log("Decrement Value " + list);
+}

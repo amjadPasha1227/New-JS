11722eefbed34d7b88ff3105ee7e93c6a8785654
amjadpasha1227 <amjadpasha1227@gmail.com>
2022-06-23 23:06:42 +0530

    new file

---
+++ Practice_or_Tasks/04.pyramid_pattern.js
@@ -0,0 +1,21 @@
+let i, j, string = '';
+
+for(i=1; i<=5; i++){
+    for(j=1; j<=i; j++){
+        string += "*";
+    }
+    string += "\n"
+}
+console.log(string);
+
+/*
+output
+
+*
+**
+***
+****
+*****
+
+*/
+

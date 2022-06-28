2517ca48567315db5492af40dc44885a9a074c9a
amjadPasha1227 <amjadpasha1227@gmail.com>
2022-06-28 19:50:35 +0530

    aaaa

--- Practice_or_Tasks/05.DateandTimeDisplay.js
+++ Practice_or_Tasks/05.DateandTimeDisplay.js
@@ -3,14 +3,16 @@ function realTimeClock(){
 let HRS = document.getElementById("hours"),
 MTS = document.getElementById("minutes"),
 SEC = document.getElementById("seconds"),
-Date = document.getElementById("date");
-
-
+DATE = document.getElementById("date");
+MONTH = document.getElementById("month");
+YEAR = document.getElementById("year");
 
 HRS.innerHTML = new Date().getHours();
 MTS.innerHTML = new Date().getMinutes();
 SEC.innerHTML = new Date().getSeconds();
-Date.innerHTML = new Date().getDate();
+DATE.innerHTML = new Date().getDate();
+MONTH.innerHTML = new Date().getMonth();
+YEAR.innerHTML = new Date().getFullYear();
 var t = setTimeout(realTimeClock, 500);
 
 }

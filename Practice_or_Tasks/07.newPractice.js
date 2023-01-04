de515bd7315dfcc51fde09c2fd6f155dfd4cd58d
amjadpasha <amjadpasham@innvectra.com>
2023-01-05 00:51:56 +0530

    New files

---
+++ Practice_or_Tasks/07.newPractice.js
@@ -0,0 +1,26 @@
+// if (false || true){
+//     console.log("Hi")
+// }
+
+////////////////////////
+
+// let age = 100;
+
+// age < 45 ? console.log("HI") :
+// age < 65 ? console.log("Hello") :
+// console.log("Old Age")
+
+//////////////////////////////
+
+// let str = "an arbitrary string instead of a number"
+// console.log(typeof str);
+
+// str = Number(str);
+// console.log(typeof str);
+// console.log(str);
+// console.log(typeof str);
+
+////////////////////////////
+console.log(null == undefined)
+console.log(null === undefined)
+
